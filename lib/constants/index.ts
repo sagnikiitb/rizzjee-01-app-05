bae4f7b56325d4ef6380148aaebc1c7080fa4e7a
ifracti <81822210+sagnikiitb@users.noreply.github.com>
2025-04-25 16:32:50 +0530

    Update index.ts C1

--- lib/constants/index.ts
+++ lib/constants/index.ts
@@ -1,1 +1,7 @@
 export const CHAT_ID = 'search' as const
+// So from what I understand, this file SEPARATELY DEFINES just the CHAT_ID parameter
+// One entire file just for one thing
+// Code modularity
+// 'as const' this ensures stronger type-safety, as in now CHAT_ID is not just a string anymore
+// It has a separate type defined as 'search'
+// Which extends the 'string' type
