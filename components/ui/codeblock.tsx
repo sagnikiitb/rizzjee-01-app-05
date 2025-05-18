4d8323c0355913d344276b412c544d6e210fb654
ifracti <81822210+sagnikiitb@users.noreply.github.com>
2025-05-18 20:41:41 +0530

    Update codeblock.tsx css

--- components/ui/codeblock.tsx
+++ components/ui/codeblock.tsx
@@ -653,6 +653,9 @@ fontFamily: 'var(--font-mono)'
           </pre>
         </div>
       )}
+      <div className="w-full h-[500px] border border-gray-300 rounded-lg shadow-md bg-white">
+        <PlotlyGraph data={plot_data} layout={plot_layout} />
+      </div>
 
       
     </div>
