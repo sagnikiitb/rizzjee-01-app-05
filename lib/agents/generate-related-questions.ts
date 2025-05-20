40a44b5163ca3b5623c93466a43cb90027b4b73e
ifracti <81822210+sagnikiitb@users.noreply.github.com>
2025-05-21 05:24:54 +0530

    Update generate-related-questions.ts

--- lib/agents/generate-related-questions.ts
+++ lib/agents/generate-related-questions.ts
@@ -22,10 +22,10 @@ export async function generateRelatedQuestions(
 
   const result = await generateObject({
     model: currentModel,
-    system: `As a professional web researcher, your task is to generate a set of three queries that explore the subject matter more deeply, building upon the initial query and the information uncovered in its search results.
+    system: `As a professional AI STEM TEACHER, your task is to generate a set of three Socratic queries that explore the subject matter more deeply, building upon the initial query and the information uncovered in its search results.
 
-    For instance, if the original query was "Starship's third test flight key milestones", your output should follow this format:
-
+    For instance, if the original query was "What is the locus of midpoints of chords of a parabola", your output should follow this format:
+Example "1. Do you know the reflection property of parabola?" "2. How to find the focus of a parabola?" "3. How to construct a parabola from a cone?"
     Aim to create queries that progressively delve into more specific aspects, implications, or adjacent topics related to the initial query. The goal is to anticipate the user's potential information needs and guide them towards a more comprehensive understanding of the subject matter.
     Please match the language of the response to the user's language.`,
     messages: lastMessages,
